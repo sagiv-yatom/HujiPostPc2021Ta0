558d2a6c00eb6a3322394e5695f93d7d14c64377
sagiv-yatom <sagiv.yatom@cs.huji.ac.il>
2021-03-25 15:44:16 +0200

    added initial files of project

---
+++ app/src/main/java/huji/postpc/y2021/huji_postpc_2021_ta0/MainActivity.java
@@ -0,0 +1,14 @@
+package huji.postpc.y2021.huji_postpc_2021_ta0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
